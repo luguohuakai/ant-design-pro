572dc35fe10cbef1a195c57b957e5c0769a8f0ef
Administrator <1318348922@qq.com>
2018-07-07 19:46:01 +0800

    用户

--- src/routes/Lst/UserLst.js
+++ src/routes/Lst/UserLst.js
@@ -1,8 +1,66 @@
-import React from 'react';
-import { Link } from 'dva/router';
+/* eslint-disable linebreak-style */
+import React, { PureComponent } from 'react';
 
-export default () => (
-  <div>
-    <h1>good</h1>
-  </div>
-);
+import {Table ,Input,List, Card} from 'antd';
+import PageHeaderLayout from '../../layouts/PageHeaderLayout';
+
+const { Search } = Input;
+import styles from './UserLst.less';
+export default class BasicList extends PureComponent {
+    render() {
+        const columns = [{
+            title: 'Name',
+            dataIndex: 'name',
+        }, {
+            title: 'Age',
+            dataIndex: 'age',
+        }, {
+            title: 'Address',
+            dataIndex: 'address',
+        }];
+        const extraContent = (
+            <div className={styles.extraContent}>
+                <Search
+                    id="search"
+                    className={styles.extraContentSearch}
+                    placeholder="请輸入"
+                    // onSearch={handleSearch}
+                />
+            </div>
+        );
+        const data = [{
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+        }, {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park',
+        }, {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park',
+        }];
+        return (
+            <PageHeaderLayout>
+                <div className={styles.standardList}>
+                    <Card
+                        className={styles.listCard}
+                        bordered={false}
+                        title="用戶列表"
+                        style={{ marginTop: 24 }}
+                        bodyStyle={{ padding: '0 32px 40px 32px' }}
+                        extra={extraContent}
+                    >
+                    <h4>用戶列表</h4>
+                    <Table columns={columns} dataSource={data} size="middle" />
+                    </Card>
+                </div>
+            </PageHeaderLayout>
+
+        );
+    }
+}
